363df80a64d0f67a240de8bd4d282ef10fdcec01
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-13 15:31:11 -0500

    created functions to store, display and append city search history

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,6 @@
 var weatherAPIKey = "4f19201c0368d72d25707fd34bd61aba";
 var weatherURL = "https://api.openweathermap.org";
-var trackHistory = [];
-
-//Using dayjs to get timezone ifnormation
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
+var storedCities = [];
 
 var formSearch = document.querySelector(".formSearch");
 var buttonSearch = document.querySelector(".buttonSearch");
@@ -14,3 +10,63 @@ var dailyEncap = document.querySelector(".daily");
 var forecastEncap = document.querySelector(".forecast");
 
 
+function displayStoredCities() {
+    historyEncap.innerHTML ="";
+
+    for (var i = 0; i < storedCities.length - 1; i++) {
+      var btn = document.createElement('button');
+      btn.setAttribute('type', 'button');
+      btn.classList.add('history-btn', 'btn-history');
+  
+
+      btn.setAttribute('data-search', storedCities[i]);
+      btn.textContent = storedCities[i];
+      historyEncap.append(btn);
+    }
+}
+
+function addtoStoredCities(userInput) {
+    // If there is no search term return the function
+    if (storedCities.indexOf(userInput) !== -1) {
+      return;
+    }
+    storedCities.push(userInput);
+  
+    localStorage.setItem('city-history', JSON.stringify(storedCities));
+    displayStoredCities();
+  }
+
+function grabStoredCities() {
+    var cityHistory = localStorage.getItem('city-history');
+    if (cityHistory) {
+        storedCities = JSON.parse(cityHistory);
+    }
+    displayStoredCities();
+}
+
+function handleSearchFormSubmit(e) {
+    // Don't continue if there is nothing in the search form
+    if (!inputSearch.value) {
+      return;
+    }
+  
+    e.preventDefault();
+    var userInput = inputSearch.value.trim();
+    fetchCoords(userInput);
+    inputSearch.value = '';
+}
+
+function handleSearchHistoryClick(e) {
+    // Don't do search if current elements is not a search history button
+    if (!e.target.matches('.btn-history')) {
+      return;
+    }
+  
+    var btn = e.target;
+    var userInput = btn.getAttribute('data-search');
+    fetchCoords(userInput);
+}
+
+grabStoredCities();
+formSearch.addEventListener('submit', handleSearchFormSubmit);
+historyEncap.addEventListener('click', handleSearchHistoryClick);
