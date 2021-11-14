5260cf43f4a5e515f59525ffeb2e404e5fa6b012
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-13 19:18:19 -0500

    got GetWeather fx to work

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,7 +34,7 @@ function addToStoredCities(userInput) {
   
     localStorage.setItem('city-history', JSON.stringify(storedCities));
     displayStoredCities();
-  }
+}
 
 function grabStoredCities() {
     var cityHistory = localStorage.getItem('city-history');
@@ -56,6 +56,7 @@ function getCityLatLong(userInput) {
           alert(`Could not find a city named ${userInput}`);
         } else {
           addToStoredCities(userInput);
+          getWeather(data[0]);
         }
       })
   }
@@ -81,18 +82,92 @@ function cityHistoryLinks(e) {
     getCityLatLong(userInput);
 }
 
-function getWeatherDetails(lat, lon, city) {
-    var apiUrl = `${weatherURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+// function getWeatherDetails(lat, lon, city) {
+//     var apiUrl = `${weatherURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
+//     fetch(apiUrl)
+//     .then(function(res) {
+//         return res.json();
+//     })
+//     .then(function (data) {   
+//         displayWeatherDetails(city, data)
+//     })
+// }
+
+// function displayWeatherDetails(userInput) {
+//     var lat = userInput.l
+// }
+
+function getWeather(location) {
+    dailyEncap.innerHTML = "";
+    forecastEncap.innerHTML ="<h2></h2>";
+    var lat = location.lat;
+    var lon = location.lon;
+    var city = location.name;
+    var apiUrl = `${weatherURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  
     fetch(apiUrl)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function (data) {   
-        displayData(data)
-    })
-    $("#cityName").text(city.toUpperCase() +" " + moment().format("MM/DD/YYYY"))
-}
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        $("<div>", { class: "row w-100 topBorder" })
+          .append(
+            $(
+              "<div class='col'><h2>" +
+                city +
+                " (" +
+                moment.unix(data.current.dt).format("M/DD/YYYY") +
+                ")</h2><p>Temp: " +
+                Math.round(data.current.temp) +
+                "°F</p><p>Wind: " +
+                Math.round(data.current.wind_speed) +
+                " MPH</p><p>Humidity: " +
+                data.current.humidity +
+                "%</p><p>UV Index: <span id='topUvi' class='p-1 rounded text-dark'>" +
+                data.current.uvi +
+                "</span></p></div><div class='col'><img src='https://openweathermap.org/img/wn/" +
+                data.current.weather[0].icon +
+                "@4x.png' alt='weather icon'></div></div>"
+            )
+          )
+          .appendTo($("#daily"));
+        if (data.current.uvi <= 2) {
+          $("#topUvi").css("background-color", "green");
+        } else if (data.current.uvi <= 5) {
+          $("#topUvi").css("background-color", "yellow");
+        } else if (data.current.uvi <= 7) {
+          $("#topUvi").css("background-color", "orange");
+        } else if (data.current.uvi <= 10) {
+          $("#topUvi").css("background-color", "red");
+        } else {
+          $("#topUvi").css("background-color", "purple");
+        }
+        $("#forecast").children("h2").text("5-Day Forecast");
+        for (var i = 0; i < 5; i++) {
+          $("<div>", { class: "col w-20 d-inline-flex" })
+            .append(
+              $(
+                "<div class='card w-20'><img class='card-img-top mw-20' src='https://openweathermap.org/img/wn/" +
+                  data.daily[i].weather[0].icon +
+                  "@4x.png' alt='weather icon'><div class='card-body'><h5 class='card-title'>" +
+                  moment.unix(data.daily[i].dt).format("M/DD/YYYY") +
+                  "</h5><p class='card-text'>Temp: " +
+                  Math.round(data.daily[i].temp.max) +
+                  "°F</p><p class='card-text'>Wind: " +
+                  Math.round(data.daily[i].wind_speed) +
+                  " MPH</p><p class='card-text'>Humidity: " +
+                  data.daily[i].humidity +
+                  "%</p></div></div></div>"
+              )
+            )
+            .appendTo($("#forecast"));
+        }
+        grabStoredCities();
+    });
+  }
+
+
 
 grabStoredCities();
 formSearch.addEventListener('submit', citySearchSubmit);
